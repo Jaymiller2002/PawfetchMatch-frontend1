37f04c0ccb6f081418ccdbd783662bd593ad1be9
Jaymiller2002 <jay.miller02@icloud.com>
2024-06-17 15:08:13 -0400

    added images to all Profiles so now you can see each users profile image

--- src/AllProfiles.jsx
+++ src/AllProfiles.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect, useContext } from 'react';
 import { fetchAllProfiles } from './api'; // Adjust the path to match your API file
 import { AuthContext } from './context'; // Adjust the path to your AuthContext
 import './AllProfiles.css';
+import { useNavigate } from 'react-router-dom';
 
 const AllProfiles = () => {
   const [profiles, setProfiles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProfiles = async () => {
@@ -28,9 +30,15 @@ const AllProfiles = () => {
     }
   }, [auth.accessToken]); // Dependency array ensures fetchProfiles is called when auth.accessToken changes
 
+  useEffect(() => {
+    if (!auth.accessToken) {
+        navigate('/login');
+    }
+}, [auth.accessToken, navigate]);
+
   return (
     <div className='all-profiles-container'>
-      <h1 className='page-title'>All Profiles</h1>
+      <h1 className='page-title'></h1>
       {loading ? (
         <p className='loading-message'>Loading...</p>
       ) : error ? (
@@ -39,7 +47,9 @@ const AllProfiles = () => {
         <ul className='profile-list'>
           {profiles.map(profile => (
             <li key={profile.id} className='profile-item'>
-              <strong>{profile.first_name} - ID: {profile.user}</strong>: {profile.bio}
+              <img className="profile-image" src={`http://127.0.0.1:8000${profile.image}`} alt="User Profile" />
+              <br />
+              <strong>{profile.first_name} {profile.last_name} Id: {profile.user}</strong>: {profile.bio}
               {/* Add profile.image later (just has the link names not the actual image) */}
             </li>
           ))}
