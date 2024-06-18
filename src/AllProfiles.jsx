d1adc34e372b266e6c804fc37c9da50bbdf704ae
Jaymiller2002 <jay.miller02@icloud.com>
2024-06-18 17:06:13 -0400

    work?

--- src/AllProfiles.jsx
+++ src/AllProfiles.jsx
@@ -47,7 +47,7 @@ const AllProfiles = () => {
         <ul className='profile-list'>
           {profiles.map(profile => (
             <li key={profile.id} className='profile-item'>
-              <img className="profile-image" src={`${baseUrl}${profile.image}`} alt="User Profile" />
+              <img className="profile-image" src={`${baseUrl}/${profile.image}`} alt="User Profile" />
               <div className="profile-details">
                 <div><strong>{profile.first_name} {profile.last_name}</strong></div>
                 <div><strong>Id: {profile.user}</strong></div>
