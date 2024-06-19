0e0115e780fe049c773fe9c94319ada00d1d9eb8
Jaymiller2002 <jay.miller02@icloud.com>
2024-06-19 14:20:41 -0400

    added a footer with a link to my personal portfolio along with my email, phone number and some other things. Dont break pls

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,24 @@
+import './App.css'
+
 function Footer() {
   return (
-    <div>
-    </div>
-  )
+    <footer>
+      <div>
+        <p>&copy; {new Date().getFullYear()} Doglist. All rights reserved.</p>
+        <p>342 E Main St, Lexington, KY</p>
+        <p>Email: jay.miller02@icloud.com | Phone: (859) 475-8431</p>
+      </div>
+      <div>
+        <a
+          href="https://portfolio-landing-page-o5j8sij4y-jaymiller2002s-projects.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check out my portfolio project
+        </a>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
