f8caca119a9cea1a478bbca43fd2f0c1cb3c27dd
Jaymiller2002 <jay.miller02@icloud.com>
2024-06-21 10:49:38 -0400

    lets see, probably wont work tbh

--- src/GetMessages.jsx
+++ src/GetMessages.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from './context';
 import { getMessages } from './api'; // Assuming getMessages function is defined in api.js
 import { useNavigate } from 'react-router-dom';
-import './GetMessages.css'
+import './GetMessages.css';
 import { baseUrl } from './api';
 
 function GetMessages() {
@@ -13,19 +13,19 @@ function GetMessages() {
   useEffect(() => {
     fetchData();
     // Polling for updates every 10 seconds
-    const interval = setInterval(fetchData, 5000);
+    const interval = setInterval(fetchData, 10000); // Adjust interval to match requirements
     return () => clearInterval(interval);
   }, [auth.accessToken]);
 
   const fetchData = async () => {
     if (auth.accessToken) {
-        try {
-            const response = await getMessages({ auth });
-            console.log("Got Messages: ", response)
-            setMessages(response.data); // Assuming response.data contains the messages
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-        }
+      try {
+        const response = await getMessages({ auth });
+        console.log("Got Messages: ", response.data);
+        setMessages(response.data); // Assuming response.data contains the messages
+      } catch (error) {
+        console.error('Error fetching messages:', error);
+      }
     }
   };
 
@@ -41,9 +41,9 @@ function GetMessages() {
       <h2>Sent/Received Messages</h2>
       {messages.map(message => (
         <div key={message.id} className="message">
-          <div className='message-sender'>Sender: {message.sender}</div>
+          <div className='message-sender'>Sender: {message.sender.username}</div>
           <div className='message-content'>Content: {message.content}</div>
-          <img src={`${baseUrl}${message.image}`} alt="Message Image" className='message-image'/>
+          {message.image && <img src={`${baseUrl}${message.image}`} alt="Message Image" className='message-image'/>}
         </div>
       ))}
     </div>
